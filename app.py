0a4d76687f6e66f88d5a5362104316263a2de24e
flavioelavin-coder <flavioe.lavin@gmail.com>
2025-08-27 10:44:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,13 @@ hist_button = st.button('Construir histograma')
      
 if hist_button: 
          st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-         
-        
-fig = px.histogram(car_data, x="odometer")
+         fig = px.histogram(car_data, x="odometer")
      
-         
+    
 st.plotly_chart(fig, use_container_width=True)
      
 st.subheader("📊 Datos generados")
-st.dataframe(df.head())
+st.dataframe(car_data.head())
 
 car_data = pd.read_csv('vehicles_us.csv') 
 fig = px.scatter(car_data, x="odometer", y="price") 
