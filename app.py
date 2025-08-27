20c8da775343adba0050e45e791f8e43ae73c497
flavioelavin-coder <flavioe.lavin@gmail.com>
2025-08-27 16:42:34 -0700

    n

--- app.py
+++ app.py
@@ -3,22 +3,6 @@ import plotly.express as px
 import streamlit as st
 import numpy as np
 
-st.title("Datos de anuncios de coches")
-     
-
-    
-car_data = pd.read_csv('vehicles_us.csv') 
-hist_button = st.button('Construir histograma') 
-     
-if hist_button: 
-         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-         fig = px.histogram(car_data, x="odometer")
-         st.plotly_chart(fig, use_container_width=True)
-
-     
-st.subheader("📊 Datos generados")
-st.dataframe(car_data.head())
-
 car_data = pd.read_csv('vehicles_us.csv') 
 
 st.title("Vehicle types by manufacturer")
@@ -36,3 +20,18 @@ fig = px.histogram(
 )
 
 fig.show()
+    
+car_data = pd.read_csv('vehicles_us.csv') 
+hist_button = st.button('Construir histograma') 
+     
+if hist_button: 
+         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         fig = px.histogram(car_data, x="odometer")
+         st.plotly_chart(fig, use_container_width=True)
+
+     
+st.subheader("📊 Datos generados")
+st.dataframe(car_data.head())
+
+
+
