a79077e911cfb00002186a4fad36ad6109ace8f0
flavioelavin-coder <flavioe.lavin@gmail.com>
2025-08-27 15:17:30 -0700

    Actualizacion

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if hist_button:
          st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          fig = px.histogram(car_data, x="odometer")
          st.plotly_chart(fig, use_container_width=True)
-         
+
      
 st.subheader("📊 Datos generados")
 st.dataframe(car_data.head())
@@ -26,3 +26,6 @@ fig.show()
 car_data = pd.read_csv('vehicles_us.csv') 
 fig = px.scatter(car_data, x="odometer", y="price") 
 fig.show() 
+
+fig = px.scatter(car_data, x="odometer", y="price") 
+fig.show()
